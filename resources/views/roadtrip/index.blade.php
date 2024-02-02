a558927d77c2a76e834f66454dff03afdb20d820
FakeSanta <aurelienfevrier08@gmail.com>
2024-02-02 14:35:24 +0100

    hiking layer

--- resources/views/roadtrip/index.blade.php
+++ resources/views/roadtrip/index.blade.php
@@ -58,7 +58,7 @@
                                 attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
                                 ext: 'png'
                             }),
-                            L.tileLayer('https://tile.waymarkedtrails.org/slopes/{z}/{x}/{y}.png', {
+                            L.tileLayer('https://tile.waymarkedtrails.org/hiking/{z}/{x}/{y}.png', {
                                 maxZoom: 18,
                                 attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Map style: &copy; <a href="https://waymarkedtrails.org">waymarkedtrails.org</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
                             })
